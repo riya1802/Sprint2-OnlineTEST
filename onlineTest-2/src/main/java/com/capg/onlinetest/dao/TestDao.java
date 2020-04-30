9430203a77db26b33636061c53486f674015074b
Sakshidrabu <drabusakshi@gmail.com>
2020-04-30 12:56:10 +0530

    Test Dao by Sakshi Drabu

--- onlineTest-2/src/main/java/com/capg/onlinetest/dao/TestDao.java
+++ onlineTest-2/src/main/java/com/capg/onlinetest/dao/TestDao.java
@@ -1,9 +1,9 @@
-package com.capg.onlinetest.dao;
+package com.example.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.capg.onlinetest.model.Test;
+import com.example.model.Test;
 
 @Repository
 public interface TestDao extends JpaRepository<Test, Integer>{
