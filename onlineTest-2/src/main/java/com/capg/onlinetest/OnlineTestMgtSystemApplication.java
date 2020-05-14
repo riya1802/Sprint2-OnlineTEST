e68ecca3ce79afcdf8c65ac6d3e26c295d5e7416
Piyush Singh Thakur <piyushsngh636@gmail.com>
2020-05-14 23:41:47 +0530

    EurekaClient

--- onlineTest-2/src/main/java/com/capg/onlinetest/OnlineTestMgtSystemApplication.java
+++ onlineTest-2/src/main/java/com/capg/onlinetest/OnlineTestMgtSystemApplication.java
@@ -2,6 +2,7 @@ package com.capg.onlinetest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -9,6 +10,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OnlineTestMgtSystemApplication {
 
